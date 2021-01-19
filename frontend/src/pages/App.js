ca7a962fdfffa55c42e32dd860fd4e36b9d280f3
Kim234 <djehoaos@ajou.ac.kr>
2021-01-19 21:57:47 +0900

    second commit

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -11,8 +11,12 @@ class App extends Component {
   constructor(props){   // render()보다 먼저 실행하여 초기화를 담당한다.
     super(props);
   }
+
+  onChange(e){
+    
+  }
+
   render(){
-    let resultform;
 
     return (
       <div className="App">
@@ -21,8 +25,10 @@ class App extends Component {
        </div>
 
        <Router>
-          <Route exact path='/' component={FirstQuestion}></Route>
-          <Route exact path='/SecondQuestion' component={SecondQuestion}></Route>
+          <Route exact path='/' render={()=><FirstQuestion />}/>
+          <Route exact path='/SecondQuestion' render={()=><SecondQuestion  />}/>
+          <Route exact path='/ThirdQuestion' render={()=><ThirdQuestion />}/>
+          <Route exact path='/FourthQuestion' render={()=><FourthQuestion />}/>
         </Router>
       
       </div>
